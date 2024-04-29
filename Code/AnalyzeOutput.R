a69ccb9ac8a3d28780743a67023ffc94542749ee
Astrid <astrid.stulemeijer@gmail.com>
2024-04-29 21:33:39 +0200

    Update second observations simulation

--- Code/AnalyzeOutput.R
+++ Code/AnalyzeOutput.R
@@ -3,16 +3,15 @@
 ###
 
 NetworkSummary <- function(networklist){
-  numerics <- lapply(networklist, function(x) matrix(as.numeric(x), nrow = nrow(x)))
-  netwobjects <- lapply(numerics, function(x) 
-    network(x, directed = T, matrix.type = "adjacency"))
+  netwobjects <- lapply(networklist, function(x) 
+    network(as.matrix(x), directed = T, matrix.type = "adjacency"))
   
   netwstats <- data.frame(
-    "Network size" = mean(sapply(netwobjects, network.size)),
-    "Density" = mean(sapply(netwobjects, gden)),
-    "Components" = mean(sapply(netwobjects, components)),
-    "Transitivity" = mean(sapply(netwobjects, function(x) gtrans(x, mode = "graph"))),
-    "Reciprocity" = mean(sapply(netwobjects, function(x) grecip (x, measure = "edgewise")))
+    "Network size" = sapply(netwobjects, network.size),
+    "Density" = sapply(netwobjects, gden),
+    "Components" = sapply(netwobjects, components),
+    "Transitivity" = sapply(netwobjects, function(x) gtrans(x, mode = "graph")),
+    "Reciprocity" = sapply(netwobjects, function(x) grecip (x, measure = "edgewise"))
   )
   
   return(netwstats)
@@ -20,14 +19,13 @@ NetworkSummary <- function(networklist){
 
 AnalyzeOutput <- function(output, realmodel, n, error_combo){
   
-  for (i in 1:ncol(error_combo)){
-    indices2 <- ((i-1)*(n)+1):n*i
-    indices <- 1:75
+  for (i in 1:nrow(error_combo)){
+    indices <- ((i-1)*(n)+1):n*i
     obs_1 <- lapply(output$original_networks[indices], function(arr) arr[,,1])
     obs_2 <- lapply(output$original_networks[indices], function(arr) arr[,,2])
     an_1 <- lapply(output$analyzed_networks[indices], function(arr) arr[,,1])
     an_2 <- lapply(output$analyzed_networks[indices], function(arr) arr[,,2])
     
-    print(NetworkSummary(obs_1))
+    View(NetworkSummary(obs_2))
   }
 }
